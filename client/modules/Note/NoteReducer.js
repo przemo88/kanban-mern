f7744b67b09514fb7ef2e6cb2289ce9d35b45692
Przemyslaw Przewoznik <blagoff88@gmail.com>
2019-05-24 19:28:53 +0200

    id error

--- client/modules/Note/NoteReducer.js
+++ client/modules/Note/NoteReducer.js
@@ -12,8 +12,8 @@ export default function notes(state = initialState, action) {
     case UPDATE_NOTE:
       return { ...state, [action.note.id]: action.note };
     case EDIT_NOTE: {
-      const note = { ...state[action.id], editing: true };
-      return { ...state, [action.id]: note };
+      const note = { ...state[action.noteId], editing: true };
+      return { ...state, [action.noteId]: note };
     }
     case DELETE_NOTE:
       return omit(state, action.noteId);
